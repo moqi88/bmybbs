a5502446466f7200e1f0c38b1f7faf40752b422b
pzhg <pzhg@208aad91-a122-71c1-c3cf-5c64c101783c>
2011-01-20 13:43:53 +0000

    postfile

--- local_utl/postfile.c
+++ local_utl/postfile.c
@@ -1,10 +1,14 @@
 #include "bbs.h"
+#include "ythtbbs.h"
 #include <stdio.h>
+ 
+ 
+
 
 int main(int argc, char **argv)
 {
-	//postfile file boardname title
+	//postfile file user boardname title
 	
-	int ret=postfile(argv[1], argv[2], argv[3], 1);
+	int ret=postfile(argv[1], argv[2], argv[3], argv[4]);
 	return 0;
 }
