4ce8b8e9b4cc449069d9959718bc53010f0a18a6
pzhg <pzhg@208aad91-a122-71c1-c3cf-5c64c101783c>
2011-01-20 13:53:19 +0000

    测试自动更新日志效果

--- local_utl/postfile.c
+++ local_utl/postfile.c
@@ -8,6 +8,11 @@
 int main(int argc, char **argv)
 {
 	//postfile file user boardname title
+	if (argc != 4 && argc!=5) {
+	  printf("usage: ./postfile file author board title\n");
+	  printf("which means post \"file\" to \"board\", with the \"title\" and \"author\"\n");
+	  return -1;
+     }
 	
 	int ret=postfile(argv[1], argv[2], argv[3], argv[4]);
 	return 0;
