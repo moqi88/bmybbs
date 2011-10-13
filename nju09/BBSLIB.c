9cad0554baf2e1c62e267eff5c2abf1777b57c83
IB <IB@208aad91-a122-71c1-c3cf-5c64c101783c>
2011-10-13 07:55:49 +0000

    1. 加入libiconv-1.14源代码 2. 新增 gb2312/utf8 互转的方法 3. 在 nju09 的链接中加入 -liconv 选项 4. 删除 libghthash-0.5.0 文件夹

--- nju09/BBSLIB.c
+++ nju09/BBSLIB.c
@@ -3144,3 +3144,49 @@ char *buf, *boardname, *filename;
 	return buf;
 }
 
+int gb2312_to_utf8(char *in, char *out, size_t size){
+	iconv_t cd;
+	cd = iconv_open("UTF-8", "GB2312");
+	if ( cd == (iconv_t)(-1) ){
+		perror("iconv_open failed");
+		return 0;
+	}
+
+	size_t in_left = strlen(in) + 1;
+	char *out_ptr;
+	size_t res;
+
+	out_ptr = out;
+	res = iconv(cd, &in, &in_left, &out_ptr, &size);
+	if ( res == (size_t)(-1) ){
+		perror("iconv failed");
+		return 0;
+	}
+
+	iconv_close(cd);
+	return 1;
+}
+
+int utf8_to_gb2312(char *in, char *out, size_t size){
+	iconv_t cd;
+	cd = iconv_open("GB2312", "UTF-8");
+	if ( cd == (iconv_t)(-1) ){
+		perror("iconv_open failed");
+		return 0;
+	}
+
+	size_t in_left = strlen(in) + 1;
+	char *out_ptr;
+	size_t res;
+
+	out_ptr = out;
+	res = iconv(cd, &in, &in_left, &out_ptr, &size);
+	if ( res == (size_t)(-1) ){
+		perror("iconv failed");
+		return 0;
+	}
+
+	iconv_close(cd);
+	return 1;
+}
+
