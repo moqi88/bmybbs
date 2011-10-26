4b41120d217077f48e80eaaa70784cbdeedcc4d4
pzhg <pzhg@208aad91-a122-71c1-c3cf-5c64c101783c>
2011-10-26 15:50:08 +0000

    滚动区高度

--- nju09/bbsboa.c
+++ nju09/bbsboa.c
@@ -821,7 +821,7 @@ int show_content()
 	}
 	bzero(buf1, 512);
 	bzero(buf2, 512);
-	printf("<marquee scrollamount=1 scrolldelay=30 direction= UP width=200>\n");
+	printf("<marquee scrollamount=1 scrolldelay=20 direction= UP width=140 hright=80>\n");
 	while(fgets(buf1, 512, fp)){
 		strltrim(strrtrim(buf1));
 		if (strlen(buf1) <= 1)
