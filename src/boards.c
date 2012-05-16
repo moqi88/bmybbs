fe442861b346255bffac76c7bfc72eabcdda5123
IB <IB@208aad91-a122-71c1-c3cf-5c64c101783c>
2012-05-16 11:06:25 +0000

    索引越界

--- src/boards.c
+++ src/boards.c
@@ -1682,7 +1682,7 @@ char buf[512];
 	    else if(ent->accessed & FH_DIGEST) strcpy(msg,"\033[1;33m[推荐]\033[0m");
 	    else if(ent->accessed & FH_MARKED) strcpy(msg,"\033[1;36m[提示]\033[0m");
 	    else strcpy(msg,"\033[1;32m[提示]\033[0m");
-	    msg[32]=0;
+	    msg[31]=0;
 	    sprintf(buf, " %6s %-12.12s%s\033[m %s◆ %-.45s ",msg,ent->owner, date," ", TITLE);
 	}
 	//end
