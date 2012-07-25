e8dc30d84ee107d672463f02a8dec82ecfc7ecb8
IB <IB@208aad91-a122-71c1-c3cf-5c64c101783c>
2012-07-25 06:17:48 +0000

    判断字符串是否为 utf8 编码

--- libythtbbs/misc.h
+++ libythtbbs/misc.h
@@ -13,4 +13,6 @@ int init_newtracelogmsq();
 int u2g(char *inbuf,int inlen,char *outbuf,int outlen);
 int g2u(char *inbuf,size_t inlen,char *outbuf,size_t outlen);
 int code_convert(char *from_charset,char *to_charset,char *inbuf,int inlen,char *outbuf,int outlen);
+int is_utf_special_byte(unsigned char c);
+int is_utf(char * inbuf, size_t inlen);
 #endif
